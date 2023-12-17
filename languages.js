770cb1dcd69ccf0b0ebe0a2f56055c5cb2ac54a3
Yasmim Neves <yascneves@hotmail.com>
2023-12-17 12:29:31 -0300

    inserção dos arquivos

---
+++ languages.js
@@ -0,0 +1,50 @@
+const languages = [
+  {
+    no: '16',
+    name: 'English',
+    native: 'English',
+    code: 'en'
+  },
+  {
+    no: '43',
+    name: 'Portuguese',
+    native: 'Português',
+    code: 'pt'
+  },
+  {
+    no: '10',
+    name: 'Chinese (Simplified)',
+    native: '中文简体',
+    code: 'zh-CN'
+  },
+  {
+    no: '11',
+    name: 'Chinese (Traditional)',
+    native: '中文繁體',
+    code: 'zh-TW'
+  },
+  {
+    no: '20',
+    name: 'French',
+    native: 'Français',
+    code: 'fr'
+  },
+  {
+    no: '33',
+    name: 'Japanese',
+    native: '日本語',
+    code: 'ja'
+  },
+  {
+    no: '34',
+    name: 'Korean',
+    native: '한국어',
+    code: 'ko'
+  },
+  {
+    no: '49',
+    name: 'Spanish',
+    native: 'Español',
+    code: 'es'
+  }
+]
